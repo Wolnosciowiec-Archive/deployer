504bba76e41e27b626395784c7a064b7f342cf8b
Black and Red <wesoly.krzysztofa@gmail.com>
2017-07-05 00:06:14 +0200

    Set a proper time limit as this is an internal deployment application

--- web/app.php
+++ web/app.php
@@ -2,6 +2,9 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
+// 30 minutes (we expect that the deployment could take long)
+set_time_limit(60 * 30);
+
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
